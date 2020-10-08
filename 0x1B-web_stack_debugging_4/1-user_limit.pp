a5ae9a0bbdd8b09cda224094245d6340324c5daf
jlassi1 <khawlajlassi1990@gmail.com>
2020-10-08 16:51:11 +0100

    mac

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.                                                           
+exec {'hard & soft':
+path     => ['/usr/bin/'],
+command  => "sudo sed -i 's/^holberton hard /holberton hard nofile git 32000/g' /etc/security/limits.conf",
+command  => "sudo sed -i 's/^holberton soft /holberton soft nofile git 32000/g' /etc/security/limits.conf",
+provider => 'shell',
+}
+
